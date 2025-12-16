e5fcb46b435416f121b40e77330de39e4986233b
Sonam malviya <malviyasonam651@gmail.com>
2025-12-17 00:12:06 +0530

    update git

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "ec2" {
   source        = "./modules/ec2"
   ami           = "ami-0cfde0ea8edd312d4"
 
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   name          = "MyEC2Instance"
 }
 
